5db81d54512665c9dac5bf81e623249b769f3e25
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-08-08 17:55:13 +0330

    ADD_CLASS: Rotor

---
+++ Rotor.h
@@ -0,0 +1,56 @@
+#ifndef ROTOR_H
+#define ROTOR_H
+
+#include <iostream>
+#include <vector>
+
+class Rotor
+{
+private:
+	std::string name;
+
+	//show that each alphabet is connected to which one
+	std::vector<char> wiring;
+
+	//initial rotation of rotor
+	int ringSetting;
+	int position;
+
+	//the position that next rotor rotate
+	int turnoverPosition;
+
+public:
+	static const std::string ALPHABET;
+
+	Rotor(std::string _name, std::vector<char> _wiring, 
+		  int _ringSetting = 0, int _position = 0, int _turnoverPosition = 0);
+
+	std::string getName() const;
+	int getPosition() const;
+    void setPosition(int _position);
+	int getTurnoverPosition() const;
+
+	/*
+	rotate:
+		move the position forward if get to turnover return true
+	*/
+	bool rotate();
+
+	/*
+	transform:
+		transform the character with respect to direction(by enabling position and ringSetting):
+			a <-> wiring[0]
+			b <-> wiring[1]
+			   .
+			   .
+			   .
+			z <-> wiring[25]
+	*/
+	char transform(char takenCharacter, bool isBackward = false) const;
+
+	//overloaded operator++ for rotation
+	bool operator++(int);
+
+};
+
+#endif
