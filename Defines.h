35c383425391537a4d94aaecc485262108e8ef15
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-08-09 19:57:50 +0330

    ADD_FUNCTION: enableVirtualTerminal for older windows terminal  to be able to process the ansi coler codes

--- Defines.h
+++ Defines.h
@@ -1,6 +1,8 @@
 #ifndef DEFINES_HPP
 #define DEFINES_HPP
 
+#include <windows.h>
+
 // --- ANSI Color Codes ---
 #define RESET   "\033[0m"
 #define BOLD    "\033[1m"
@@ -12,5 +14,22 @@
 #define CYAN    "\033[36m"
 #define WHITE   "\033[37m"
 #define ORANGE  "\033[38;5;208m" 
+
+#ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#endif
+
+/*
+enableVirtualTerminal:
+    make windows terminal able to read ansi coler code
+*/
+void enableVirtualTerminal()  
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);  //get curent console mode and write it in dwmode
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING; // add VT token prossesing activator to dwmode with OR
+    SetConsoleMode(hOut, dwMode); //set the console mode to dwmode
+}
 
 #endif
